12f706db52f255f3897c9c655edcf7b66471778d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-04-12 01:32:53 +0200

    Conversion Tuple classes

---
+++ src/THNETII.Common/ConversionTuple.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using THNETII.Common.Collections.Generic;
+
+namespace THNETII.Common
+{
+    public class ConversionTuple<TRaw, TConvert>
+    {
+        protected readonly object sync = new object();
+        private readonly Func<TRaw, TConvert> rawConvert;
+        private readonly Func<TRaw, TRaw, bool> rawEquals;
+        protected TRaw rawValue;
+        protected Tuple<TRaw, TConvert> cachedTuple;
+
+        public TRaw RawValue
+        {
+            get => rawValue;
+            set { lock (sync) { rawValue = value; } }
+        }
+
+        public TConvert ConvertedValue
+        {
+            get
+            {
+                TRaw localRaw;
+                Tuple<TRaw, TConvert> localCached;
+                lock (sync)
+                {
+                    localRaw = rawValue;
+                    localCached = cachedTuple;
+                }
+
+                if (localCached == null || !rawEquals(localRaw, localCached.Item1))
+                {
+                    localCached = Tuple.Create(localRaw, rawConvert(localRaw));
+                    lock (sync)
+                    {
+                        if (rawEquals(rawValue, localCached.Item1))
+                            cachedTuple = localCached;
+                    }
+                }
+
+                return localCached.Item2;
+            }
+        }
+
+        protected static Func<T, T, bool> GetEqualityCheckFunction<T>()
+        {
+            if (typeof(T).GetTypeInfo().IsValueType)
+                return EqualityComparer<T>.Default.Equals;
+            else
+                return ReferenceEqualityComparer<T>.StaticEquals;
+        }
+
+        public ConversionTuple(Func<TRaw, TConvert> rawConvert) : this(rawConvert, GetEqualityCheckFunction<TRaw>()) { }
+        public ConversionTuple(Func<TRaw, TConvert> rawConvert, IEqualityComparer<TRaw> rawEqualityComparer)
+            : this(rawConvert, (rawEqualityComparer ?? throw new ArgumentNullException(nameof(rawEqualityComparer))).Equals) { }
+        protected ConversionTuple(Func<TRaw, TConvert> rawConvert, Func<TRaw, TRaw, bool> rawEquals)
+        {
+            this.rawConvert = rawConvert ?? throw new ArgumentNullException(nameof(rawConvert));
+            this.rawEquals = rawEquals ?? throw new ArgumentNullException(nameof(rawEquals));
+        }
+    }
+}
---
+++ src/THNETII.Common/DuplexConversionTuple.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace THNETII.Common
+{
+    public class DuplexConversionTuple<TRaw, TConvert> : ConversionTuple<TRaw, TConvert>
+    {
+        private readonly Func<TConvert, TConvert, bool> convertedEquals;
+        private readonly Func<TConvert, TRaw> rawReverseConvert;
+
+        new public TConvert ConvertedValue
+        {
+            get => base.ConvertedValue;
+            set
+            {
+                var localCached = cachedTuple;
+                TRaw localRaw;
+                if (convertedEquals(localCached.Item2, value))
+                    localRaw = localCached.Item1;
+                else
+                {
+                    localRaw = rawReverseConvert(value);
+                    localCached = Tuple.Create(localRaw, value);
+                }
+                lock (sync)
+                {
+                    rawValue = localRaw;
+                    cachedTuple = localCached;
+                }
+            }
+        }
+
+        public DuplexConversionTuple(Func<TRaw, TConvert> rawConvert, Func<TConvert, TRaw> rawReverseConvert)
+            : this(rawConvert, GetEqualityCheckFunction<TRaw>(), rawReverseConvert, GetEqualityCheckFunction<TConvert>()) { }
+        public DuplexConversionTuple(Func<TRaw, TConvert> rawConvert, IEqualityComparer<TRaw> rawEqualityComparer, Func<TConvert, TRaw> rawReverseConvert)
+            : this(rawConvert, (rawEqualityComparer ?? throw new ArgumentNullException(nameof(rawEqualityComparer))).Equals,
+                  rawReverseConvert, GetEqualityCheckFunction<TConvert>())
+        { }
+        public DuplexConversionTuple(Func<TRaw, TConvert> rawConvert, Func<TConvert, TRaw> rawReverseConvert, IEqualityComparer<TConvert> convertedEqualityComparer)
+            : this(rawConvert, GetEqualityCheckFunction<TRaw>(),
+                  rawReverseConvert, (convertedEqualityComparer ?? throw new ArgumentNullException(nameof(convertedEqualityComparer))).Equals)
+        { }
+        public DuplexConversionTuple(Func<TRaw, TConvert> rawConvert, IEqualityComparer<TRaw> rawEqualityComparer, Func<TConvert, TRaw> rawReverseConvert, IEqualityComparer<TConvert> convertedEqualityComparer)
+            : this(rawConvert, (rawEqualityComparer ?? throw new ArgumentNullException(nameof(rawEqualityComparer))).Equals,
+                rawReverseConvert, (convertedEqualityComparer ?? throw new ArgumentNullException(nameof(convertedEqualityComparer))).Equals)
+        { }
+
+        protected DuplexConversionTuple(Func<TRaw, TConvert> rawConvert, Func<TRaw, TRaw, bool> rawEquals,
+            Func<TConvert, TRaw> rawReverseConvert, Func<TConvert, TConvert, bool> convertedEquals)
+            : base(rawConvert, rawEquals)
+        {
+            this.rawReverseConvert = rawReverseConvert ?? throw new ArgumentNullException(nameof(rawReverseConvert));
+            this.convertedEquals = convertedEquals ?? throw new ArgumentNullException(nameof(convertedEquals));
+        }
+    }
+}
