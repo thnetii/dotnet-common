8f2a360a80a014453deaedcd5788d92e94ab038c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-04-12 01:32:41 +0200

    Reference Equality Comparer

---
+++ src/THNETII.Common/Collections.Generic/ReferenceEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace THNETII.Common.Collections.Generic
+{
+    public class ReferenceEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private static readonly ReferenceEqualityComparer<T> @default = new ReferenceEqualityComparer<T>();
+
+        public static ReferenceEqualityComparer<T> Default => @default;
+
+        public bool Equals(T x, T y) => StaticEquals(x, y);
+
+        public static bool StaticEquals(T x, T y) => ReferenceEquals(x, y);
+
+        public int GetHashCode(T obj) => obj.GetHashCode();
+    }
+}
