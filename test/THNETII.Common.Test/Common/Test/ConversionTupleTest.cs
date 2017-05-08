056e3423030abee9b7cb891369bf322eb6ad1a39
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-05-08 23:56:54 +0200

    Initial Negative Conversion Tuple Constructor Tests

--- test/THNETII.Common.Test/Common/Test/ConversionTupleTest.cs
+++ test/THNETII.Common.Test/Common/Test/ConversionTupleTest.cs
@@ -5,5 +5,16 @@ namespace THNETII.Common.Test
 {
     public class ConversionTupleTest
     {
+        [Fact]
+        public void NewConversionTupleWithNullConverterThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ConversionTuple<int, string>(rawConvert: null));
+        }
+
+        [Fact]
+        public void NewConversionTupleWithNullComparerThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ConversionTuple<int, string>(i => i.ToString(), rawEqualityComparer: null));
+        }
     }
 }
