935853e27c035bccd716fd82d8e0f2ad55b66b7e
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-07-10 16:13:45 +0200

    Argument Extensions, Null Throwing

---
+++ src/THNETII.Common/ArgumentExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace THNETII.Common
+{
+    public static class ArgumentExtensions
+    {
+        public static T ThrowIfNull<T>(this T instance, string name) where T : class
+            => instance ?? throw new ArgumentNullException(name);
+    }
+}
