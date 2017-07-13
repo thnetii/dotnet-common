1e03a03273ddc70b606335a6563423b3733473e1
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-07-14 01:56:09 +0200

    Support for Async CLI commands

---
+++ src/THNETII.Common.Cli/CliAsyncCommand.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace THNETII.Common.Cli
+{
+    public abstract class CliAsyncCommand : CliCommand
+    {
+        public CliAsyncCommand() : base() { }
+        public CliAsyncCommand(IConfiguration configuration, ILogger<CliAsyncCommand> logger = null) : base(configuration, logger) { }
+
+        public virtual Task<int> RunAsync(CommandLineApplication app)
+            => Task.FromResult(base.Run(app));
+
+        public override sealed int Run(CommandLineApplication app)
+            => RunAsync(app)?.GetAwaiter().GetResult() ?? 1;
+    }
+}
