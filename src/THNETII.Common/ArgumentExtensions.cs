a36e1c3faf0e45a83b8cf68fbd69d0a0b47572c1
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-07-14 15:58:59 +0200

    Add String Argument Extension method

--- src/THNETII.Common/ArgumentExtensions.cs
+++ src/THNETII.Common/ArgumentExtensions.cs
@@ -6,5 +6,14 @@ namespace THNETII.Common
     {
         public static T ThrowIfNull<T>(this T instance, string name) where T : class
             => instance ?? throw new ArgumentNullException(name);
+
+        /// <exception cref="ArgumentException" />
+        /// <exception cref="ArgumentNullException" />
+        public static string ThrowIfNullOrWhiteSpace(this string value, string name)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw value == null ? new ArgumentNullException(nameof(name)) : new ArgumentException("value must neither be empty, nor null, nor whitespace-only.", name);
+            return value;
+        }
     }
 }
