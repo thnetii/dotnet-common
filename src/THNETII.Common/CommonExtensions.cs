347a27d9c6e905e0ced8e6acd85e6378ec00ceb4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-08-24 01:17:23 +0200

    Common extension methods for reference types and strings

---
+++ src/THNETII.Common/CommonExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace THNETII.Common
+{
+    public static class CommonExtensions
+    {
+        public static T IfNotNull<T>(this T x, T otherwise) => x != null ? x : otherwise;
+        public static string IfNotNullOrEmpty(this string s, string otherwise) => string.IsNullOrEmpty(s) ? otherwise : s;
+        public static string IfNotNullOrWhiteSpace(this string s, string otherwise) => string.IsNullOrWhiteSpace(s) ? otherwise : s;
+    }
+}
