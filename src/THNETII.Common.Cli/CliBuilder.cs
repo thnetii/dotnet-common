f73eba45d951dd36fa426655f63750a3cd24a699
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-10-16 03:05:21 +0200

    Start reorganising CLI libraries and make use of more TDD

--- src/THNETII.Common.Cli/CliBuilder.cs
+++ src/THNETII.Common.Cli/CliBuilder.cs
@@ -4,88 +4,30 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-
+using System.Threading;
 
 namespace THNETII.Common.Cli
 {
-    using CommandExecuteAction = Action<IConfigurationBuilder, IServiceCollection, IDictionary<string, string>>;
-    using CommandBuildAction = Action<CommandLineApplication, ICollection<Action<IConfigurationBuilder, IServiceCollection, IDictionary<string, string>>>>;
-
     public class CliBuilder<TCommand> where TCommand : CliCommand
     {
-        /// <summary>
-        /// The default command-line option template string that is used for the help option in a CLI.
-        /// </summary>
-        public const string DefaultHelpTemplate = @"-?|-h|--help";
-
         /// <summary>
         /// The default command-line option template string that is used for the version option in a CLI.
         /// </summary>
         public const string DefaultVersionTemplate = @"--version";
 
-        public Assembly ExecutingAssembly { get; }
-
-        private readonly string rootNameString;
-        private readonly string rootFullNameString;
-        private readonly string rootDescriptionString;
-        private readonly string shortVersionString;
-        private readonly string longVersionString;
-
-        private CommandLineApplication CreateDefaultRootCommand()
-        {
-            return new CommandLineApplication(throwOnUnexpectedArg: false)
-            {
-                Name = rootNameString,
-                Description = rootDescriptionString,
-                FullName = rootFullNameString
-            };
-        }
-
-
-#if NETSTANDARD2_0
-        public CliBuilder(bool useEntryAssembly = true) : this(useEntryAssembly ? Assembly.GetEntryAssembly() : typeof(TCommand).Assembly) { }
-#else
-        public CliBuilder() : this(typeof(TCommand)) { }
-#endif
-
-        public CliBuilder(Type programType) : this(programType.ThrowIfNull(nameof(programType)).GetTypeInfo().Assembly) { }
-
-        public CliBuilder(Assembly executingAssembly)
-        {
-            executingAssembly.ThrowIfNull(nameof(executingAssembly));
-            var assemblyName = executingAssembly.GetName();
-            rootNameString = assemblyName.Name;
-            rootFullNameString = executingAssembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product;
-            rootDescriptionString = executingAssembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
-            string assemblyNameVersion = assemblyName.Version.ToString();
-            string informationalVersion = executingAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
-            if (string.IsNullOrWhiteSpace(informationalVersion))
-            {
-                shortVersionString = 'v' + assemblyNameVersion;
-                longVersionString = 'v' + assemblyNameVersion;
-            }
-            else
-            {
-                shortVersionString = 'v' + informationalVersion;
-                longVersionString = $"v{informationalVersion} (v{assemblyNameVersion})";
-            }
-
-            ExecutingAssembly = executingAssembly;
-        }
-
-        private readonly ICollection<CommandBuildAction> commandActions = new List<CommandBuildAction>();
-        private readonly ICollection<Action<CommandLineApplication, IServiceProvider>> preRunActions = new List<Action<CommandLineApplication, IServiceProvider>>();
-        private bool inheritedHelpOption = false;
-        private bool inheritedVersionOption = false;
+        private Action<IConfigurationBuilder> configureAction;
+        private Action<IConfiguration, IServiceCollection> configureServices;
+        private bool inheritedHelpOption;
+        private bool inheritedVersionOption;
 
         /// <summary>
         /// Adds an action to be executed prior to running a CLI command in order to add configuration sources for the Application.
         /// </summary>
         /// <param name="configureAction">The action to execute on the Application configuration builder.</param>
         /// <returns>The current instance to allow for chaining method invocations in a functional-style.</returns>
-        public CliBuilder<TCommand> Configuration(Action<IConfigurationBuilder> configureAction)
+        public CliBuilder<TCommand> AddConfiguration(Action<IConfigurationBuilder> configureAction)
         {
-            commandActions.Add((_, execActions) => execActions.Add((configBuilder, _1, _2) => configureAction?.Invoke(configBuilder)));
+            this.configureAction += configureAction;
             return this;
         }
 
@@ -95,175 +37,37 @@ namespace THNETII.Common.Cli
         /// <param name="configureServices">The action to execute on the Service Collection of the Application DI-container.</param>
         /// <returns>The current instance to allow for chaining method invocations in a functional-style.</returns>
         public CliBuilder<TCommand> ConfigureServices(Action<IServiceCollection> configureServices)
-        {
-            commandActions.Add((_, execActions) => execActions.Add((_0, services, _2) => configureServices?.Invoke(services)));
-            return this;
-        }
-
-        public CliBuilder<TCommand> PreRunCommand(Action<CommandLineApplication, IServiceProvider> preRunAction)
-        {
-            preRunActions.Add(preRunAction);
-            return this;
-        }
+            => ConfigureServices((_, services) => configureServices?.Invoke(services));
 
         /// <summary>
-        /// Adds a help option to the CLI command.
+        /// Add an action to be executed prior to running a CLI command in order to add services to the Application DI-container.
         /// </summary>
-        /// <param name="template"></param>
-        /// <param name="inherited"></param>
-        /// <returns></returns>
-        public CliBuilder<TCommand> AddHelpOption(string template = DefaultHelpTemplate, bool inherited = true)
-            => AddHelpOption(cli => cli.HelpOption(template), inherited);
-
-        protected CliBuilder<TCommand> AddHelpOption(Func<CommandLineApplication, CommandOption> helpBuilder, bool inherited)
+        /// <param name="configureServices">The action to execute on the Service Collection of the Application DI-container.</param>
+        /// <returns>The current instance to allow for chaining method invocations in a functional-style.</returns>
+        public CliBuilder<TCommand> ConfigureServices(Action<IConfiguration, IServiceCollection> configureServices)
         {
-            if (helpBuilder != null)
-            {
-                commandActions.Add((cmd, _) => helpBuilder(cmd));
-                inheritedHelpOption = inherited;
-            }
+            this.configureServices += configureServices;
             return this;
         }
 
-        public CliBuilder<TCommand> AddVersionOption(string template = DefaultVersionTemplate, bool inherited = false)
-            => AddVersionOption(cli => cli.VersionOption(template, shortVersionString, longVersionString), inherited);
+        public CliBuilder<TCommand> AddHelpOption()
+            => throw new NotImplementedException();
 
-        protected CliBuilder<TCommand> AddVersionOption(Func<CommandLineApplication, CommandOption> versionBuilder, bool inherited)
-        {
-            if (versionBuilder != null)
-            {
-                commandActions.Add((cmd, _) => versionBuilder(cmd));
-                inheritedVersionOption = inherited;
-            }
-            return this;
-        }
-
-        public CliBuilder<TCommand> AddOption(string template, string description, CommandOptionType optionType, Action<CommandOption, IDictionary<string, string>> optionReadAction)
-            => AddOption(cli => cli.Option(template, description, optionType), optionReadAction);
-        public CliBuilder<TCommand> AddOption(string template, string description, CommandOptionType optionType, bool inherited, Action<CommandOption, IDictionary<string, string>> optionReadAction)
-            => AddOption(cli => cli.Option(template, description, optionType, inherited), optionReadAction);
-        public CliBuilder<TCommand> AddOption(string template, string description, CommandOptionType optionType, Action<CommandOption> optionConfiguration, Action<CommandOption, IDictionary<string, string>> optionReadAction)
-            => AddOption(cli => cli.Option(template, description, optionType, optionConfiguration), optionReadAction);
-        public CliBuilder<TCommand> AddOption(string template, string description, CommandOptionType optionType, Action<CommandOption> optionConfiguration, bool inherited, Action<CommandOption, IDictionary<string, string>> optionReadAction)
-            => AddOption(cli => cli.Option(template, description, optionType, optionConfiguration, inherited), optionReadAction);
-
-        protected CliBuilder<TCommand> AddOption(Func<CommandLineApplication, CommandOption> optionBuilder, Action<CommandOption, IDictionary<string, string>> optionReadAction)
-        {
-            optionBuilder.ThrowIfNull(nameof(optionBuilder));
-            commandActions.Add((cmd, execActions) =>
-            {
-                var option = optionBuilder(cmd);
-                execActions.Add((_0, _1, configDict) =>
-                {
-                    if (option.OptionType == CommandOptionType.NoValue || option.HasValue())
-                        optionReadAction?.Invoke(option, configDict);
-                });
-            });
-            return this;
-        }
-
-        public CliBuilder<TCommand> AddArgument(string template, string description, Action<CommandArgument, IDictionary<string, string>> argumentReadAction)
-            => AddArgument(cli => cli.Argument(template, description), argumentReadAction);
-        public CliBuilder<TCommand> AddArgument(string template, string description, bool multipleValues, Action<CommandArgument, IDictionary<string, string>> argumentReadAction)
-            => AddArgument(cli => cli.Argument(template, description, multipleValues), argumentReadAction);
-        public CliBuilder<TCommand> AddArgument(string template, string description, Action<CommandArgument> argumentConfigure, Action<CommandArgument, IDictionary<string, string>> argumentReadAction)
-            => AddArgument(cli => cli.Argument(template, description), argumentReadAction);
-        public CliBuilder<TCommand> AddArgument(string template, string description, Action<CommandArgument> argumentConfigure, bool multipleValues, Action<CommandArgument, IDictionary<string, string>> argumentReadAction)
-            => AddArgument(cli => cli.Argument(template, description), argumentReadAction);
-
-        protected CliBuilder<TCommand> AddArgument(Func<CommandLineApplication, CommandArgument> argumentBuilder, Action<CommandArgument, IDictionary<string, string>> argumentReadAction)
-        {
-            argumentBuilder.ThrowIfNull(nameof(argumentBuilder));
-            commandActions.Add((cmd, execActions) =>
-            {
-                var argument = argumentBuilder(cmd);
-                execActions.Add((_0, _1, configDict) => argumentReadAction?.Invoke(argument, configDict));
-            });
-            return this;
-        }
-
-        public CliBuilder<TCommand> AddSubCommand<TSubCommand>(string name, Action<CliBuilder<TSubCommand>> subCliConfigure, Action<CommandLineApplication> configuration)
-            where TSubCommand : CliCommand
-            => AddSubCommand(cli => cli.Command(name, configuration), subCliConfigure);
-        public CliBuilder<TCommand> AddSubCommand<TSubCommand>(string name, Action<CliBuilder<TSubCommand>> subCliConfigure, Action<CommandLineApplication> configuration, bool throwOnUnexpectedArg)
-            where TSubCommand : CliCommand
-            => AddSubCommand(cli => cli.Command(name, configuration, throwOnUnexpectedArg), subCliConfigure);
-
-        protected CliBuilder<TCommand> AddSubCommand<TSubCommand>(Func<CommandLineApplication, CommandLineApplication> commandBuilder, Action<CliBuilder<TSubCommand>> subCliConfigure)
-            where TSubCommand : CliCommand
-        {
-            commandBuilder.ThrowIfNull(nameof(commandBuilder));
-
-            var subCliApp = new SubCliApplication<TSubCommand>(this);
-            subCliConfigure?.Invoke(subCliApp);
-            commandActions.Add((cmd, execActions) =>
-            {
-                var subCmd = commandBuilder(cmd);
-                if (inheritedHelpOption)
-                    subCmd.HelpOption(cmd.OptionHelp.Template);
-                if (inheritedVersionOption)
-                    subCmd.VersionOption(cmd.OptionVersion.Template, cmd.ShortVersionGetter, cmd.LongVersionGetter);
-                subCliApp.PrepareCommand(subCmd, execActions);
-            });
-
-            return this;
-        }
-
-        protected virtual void ExecutePreRun(CommandLineApplication command, IServiceProvider serviceProvider)
-        {
-            foreach (var preRunAction in preRunActions)
-                preRunAction?.Invoke(command, serviceProvider);
-        }
-
-        private void PrepareCommand(CommandLineApplication command, ICollection<CommandExecuteAction> executeActions)
-        {
-            foreach (var buildAction in commandActions)
-                buildAction(command, executeActions);
-
-            int Execute()
-            {
-                var serviceCollection = new ServiceCollection();
-                var configBuilder = new ConfigurationBuilder();
-                var configDict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-
-                foreach (var execAction in executeActions)
-                    execAction?.Invoke(configBuilder, serviceCollection, configDict);
-                configBuilder.AddInMemoryCollection(configDict);
-
-                serviceCollection.AddSingleton<IConfiguration>(_ => configBuilder.Build());
-                serviceCollection.AddSingleton<TCommand>();
-
-                var serviceProvider = serviceCollection.BuildServiceProvider();
-                ExecutePreRun(command, serviceProvider);
-                var cmdInstance = serviceProvider.GetRequiredService<TCommand>();
-                return cmdInstance.Run(command);
-            }
-
-            command.OnExecute((Func<int>)Execute);
-        }
+        public CliBuilder<TCommand> AddHelpOption(string template)
+            => throw new NotImplementedException();
 
         public virtual CommandLineApplication Build()
         {
-            var rootCommand = CreateDefaultRootCommand();
-            var executeActions = new List<CommandExecuteAction>(commandActions.Count);
-            PrepareCommand(rootCommand, executeActions);
-
-            return rootCommand;
+            throw new NotImplementedException();
         }
 
-        private class SubCliApplication<TSubCommand> : CliBuilder<TSubCommand> where TSubCommand : CliCommand
+        private class NestedCliBuilder<TSubCommand> : CliBuilder<TSubCommand> where TSubCommand : CliCommand
         {
             public CliBuilder<TCommand> Parent { get; }
 
-            public SubCliApplication(CliBuilder<TCommand> parent) : base()
+            public NestedCliBuilder(CliBuilder<TCommand> parent) : base()
             {
                 Parent = parent;
-            }
-
-            protected override void ExecutePreRun(CommandLineApplication command, IServiceProvider serviceProvider)
-            {
-                Parent.ExecutePreRun(command, serviceProvider);
-                base.ExecutePreRun(command, serviceProvider);
             }
         }
     }
--- test/THNETII.Common.Test/Common.Cli/Test/CliBuilderTest.cs
+++ test/THNETII.Common.Test/Common.Cli/Test/CliBuilderTest.cs
@@ -1,65 +1,172 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Xunit;
+
+using static THNETII.Common.Cli.CliBuilder<THNETII.Common.Cli.CliCommand>;
 
 namespace THNETII.Common.Cli.Test
 {
     public class CliBuilderTest
     {
-        [Fact]
-        public void CliBuilderApplicationHasNameByDefault()
+
+        const int DefaultReturnValue = 4242;
+        const string TestConfigurationKey = "TEST";
+        const string TestConfigurationValue = "TEST-CONFIG-VALUE";
+
+
+        private class CliTestCommand : CliCommand
         {
-            CommandLineApplication cliapp = new CliBuilder<CliBuilderTestCommand>()
-                .Build();
-            Assert.False(string.IsNullOrWhiteSpace(cliapp.Name));
+            public override int Run(CommandLineApplication app) => DefaultReturnValue;
+        }
+
+        private class CliTestCommandWithConfiguration : CliCommand
+        {
+            public CliTestCommandWithConfiguration(IConfiguration configuration) : base(configuration) { }
+
+            public override int Run(CommandLineApplication app)
+            {
+                Assert.NotNull(Configuration);
+                return DefaultReturnValue;
+            }
+        }
+
+        private class CliTestCommandWithConfigurationValue : CliTestCommandWithConfiguration
+        {
+            public CliTestCommandWithConfigurationValue(IConfiguration configuration) : base(configuration) { }
+
+            public override int Run(CommandLineApplication app)
+            {
+                Assert.Equal(TestConfigurationValue, Configuration?[TestConfigurationKey]);
+                return base.Run(app);
+            }
+        }
+
+        private class TestService { }
+
+        private class CliTestCommandWithTestService : CliTestCommand
+        {
+            public TestService Service { get; }
+
+            public CliTestCommandWithTestService(TestService service) : base()
+            {
+                Service = service;
+            }
+
+            public override int Run(CommandLineApplication app)
+            {
+                Assert.NotNull(Service);
+                return DefaultReturnValue;
+            }
         }
 
         [Fact]
-        public void CliBuilderApplicationHasFullNameByDefault()
+        [SuppressMessage("Microsoft.Usage", "CA1806", Justification = "Usage of constructed instance not relevant")]
+        public void ConstructorWithNoArguments()
+            => new CliBuilder<CliTestCommand>();
+
+        [Fact]
+        public void BuildWithNoBuilderSteps()
         {
-            CommandLineApplication cliapp = new CliBuilder<CliBuilderTestCommand>()
-                .Build();
-            Assert.False(string.IsNullOrWhiteSpace(cliapp.FullName));
+            var builder = new CliBuilder<CliTestCommand>();
+            var cliapp = builder.Build();
+            Assert.NotNull(cliapp);
+            Assert.Empty(cliapp.Arguments);
+            Assert.Empty(cliapp.Commands);
+            Assert.Empty(cliapp.Options);
+            Assert.Null(cliapp.OptionHelp);
+            Assert.Null(cliapp.OptionVersion);
+            Assert.Equal(DefaultReturnValue, cliapp.Execute());
         }
 
         [Fact]
-        public void CliBuilderApplicationHasDescriptionByDefault()
+        public void BuildWithConfiguration()
         {
-            CommandLineApplication cliapp = new CliBuilder<CliBuilderTestCommand>()
-                .Build();
-            Assert.False(string.IsNullOrWhiteSpace(cliapp.Description));
+            var builder = new CliBuilder<CliTestCommandWithConfiguration>();
+            var cliapp = builder.Build();
+            Assert.NotNull(cliapp);
+            Assert.Equal(DefaultReturnValue, cliapp.Execute());
         }
 
         [Fact]
-        public void CliBuilderApplicationHasExecuteFunctionByDefault()
+        public void BuildWithConfigurationWithKeyValuePair()
         {
-            CommandLineApplication cliapp = new CliBuilder<CliBuilderTestCommand>()
-               .Build();
-            Assert.NotNull(cliapp.Invoke);
+            var builder = new CliBuilder<CliTestCommandWithConfigurationValue>();
+            builder.AddConfiguration(config =>
+            {
+                config.AddInMemoryCollection(new[]
+                {
+                    new KeyValuePair<string, string>(TestConfigurationKey, TestConfigurationValue)
+                });
+            });
+            var cliapp = builder.Build();
+            Assert.NotNull(cliapp);
+            Assert.Equal(DefaultReturnValue, cliapp.Execute());
         }
 
         [Fact]
-        public void CliBuilderApplicationHasHelpOption()
+        public void AddConfigurationReturnsSameBuilderInstance()
         {
-            CommandLineApplication cliapp = new CliBuilder<CliBuilderTestCommand>()
-                .AddHelpOption()
-                .Build();
+            var builder = new CliBuilder<CliCommand>();
+            Assert.Same(builder, builder.AddConfiguration(_ => { }));
+        }
+
+        [Fact]
+        public void AddConfigurationAcceptsNullArgument()
+        {
+            var builder = new CliBuilder<CliCommand>();
+            builder.AddConfiguration(null);
+        }
+
+        [Fact]
+        public void ConfigureServicesReturnsSameBuilderInstance()
+        {
+            var builder = new CliBuilder<CliCommand>();
+            Assert.Same(builder, builder.ConfigureServices(_ => { }));
+        }
+
+        [Fact]
+        public void ConfigureServicesAcceptsNullArgument()
+        {
+            var builder = new CliBuilder<CliCommand>();
+            builder.ConfigureServices((Action<IServiceCollection>)null);
+        }
+
+        [Fact]
+        public void ConfigureServicesWithConfigurationReturnsSameBuilderInstance()
+        {
+            var builder = new CliBuilder<CliCommand>();
+            Assert.Same(builder, builder.ConfigureServices((_1, _2) => { }));
+        }
+
+        [Fact]
+        public void ConfigureServicesWithConfigurationAcceptsNullArgument()
+        {
+            var builder = new CliBuilder<CliCommand>();
+            builder.ConfigureServices((Action<IConfiguration, IServiceCollection>)null);
+        }
+
+        [Fact]
+        public void AddHelpOptionCreatesHelpOption()
+        {
+            var builder = new CliBuilder<CliCommand>();
+            builder.AddHelpOption();
+            var cliapp = builder.Build();
             Assert.NotNull(cliapp.OptionHelp);
-            Assert.Equal(CliBuilder<CliBuilderTestCommand>.DefaultHelpTemplate, cliapp.OptionHelp.Template);
         }
 
         [Fact]
-        public void CliBuilderApplicationHasVersionOption()
+        public void AddHelpOptionCreatesHelpOptionWithSpecifiedTemplate()
         {
-            CommandLineApplication cliapp = new CliBuilder<CliBuilderTestCommand>()
-                .AddVersionOption()
-                .Build();
-            Assert.NotNull(cliapp.OptionVersion);
-            Assert.Equal(CliBuilder<CliBuilderTestCommand>.DefaultVersionTemplate, cliapp.OptionVersion.Template);
-        }
-
-        private class CliBuilderTestCommand : CliCommand
-        {
-            public override int Run(CommandLineApplication app) => 0;
+            const string helpTemplate = "--test-help";
+            var builder = new CliBuilder<CliCommand>();
+            builder.AddHelpOption(helpTemplate);
+            var cliapp = builder.Build();
+            Assert.NotNull(cliapp.OptionHelp);
+            Assert.Equal(helpTemplate, cliapp.OptionHelp.Template);
         }
     }
 }
