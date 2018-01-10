a2611bf75771c8b3c693117a09c053ced85a747d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-01-10 18:56:38 +0100

    Added factorised overload for IfNotNull extension method.

--- test/THNETII.Common.Test/Common/Test/CommonExtensionsTest.cs
+++ test/THNETII.Common.Test/Common/Test/CommonExtensionsTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace THNETII.Common.Test
 {
@@ -60,6 +61,123 @@ namespace THNETII.Common.Test
         {
             string alternate = "ALTERNATE";
             Assert.Same(alternate, input.IfNotNullOrWhiteSpace(otherwise: alternate));
+        }
+
+        [Fact]
+        public void DoesNotInvokeOtherwiseIfInstanceNotNull()
+        {
+            object test = new object();
+            bool invoked = false;
+            object otherwiseFactory()
+            {
+                invoked = true;
+                return new object();
+            }
+            Assert.Same(test, test.IfNotNull(otherwiseFactory: otherwiseFactory));
+            Assert.False(invoked);
+        }
+
+        [Fact]
+        public void DoesNotInvokeOtherwiseIfStringNotEmpty()
+        {
+            string test = "TEST";
+            bool invoked = false;
+            string otherwiseFactory()
+            {
+                invoked = true;
+                return "OTHERWISE";
+            }
+            Assert.Same(test, test.IfNotNullOrEmpty(otherwiseFactory: otherwiseFactory));
+            Assert.False(invoked);
+        }
+
+        [Fact]
+        public void DoesNotInvokeOtherwiseIfStringNotEmptyOrWhiteSpace()
+        {
+            string test = "TEST";
+            bool invoked = false;
+            string otherwiseFactory()
+            {
+                invoked = true;
+                return "OTHERWISE";
+            }
+            Assert.Same(test, test.IfNotNullOrWhiteSpace(otherwiseFactory: otherwiseFactory));
+            Assert.False(invoked);
+        }
+
+        [Fact]
+        public void InvokesOtherwiseIfInstanceNull()
+        {
+            bool invoked = false;
+            object otherwiseFactory()
+            {
+                invoked = true;
+                return new object();
+            }
+            object test = null;
+            Assert.NotSame(test, test.IfNotNull(otherwiseFactory: otherwiseFactory));
+            Assert.True(invoked);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void InvokesOtherwiseIfStringNullOrEmpty(string test)
+        {
+            bool invoked = false;
+            const string otherwiseValue = "OTHERWISE";
+            string otherwiseFactory()
+            {
+                invoked = true;
+                return otherwiseValue;
+            }
+            Assert.Equal(otherwiseValue, test.IfNotNullOrEmpty(otherwiseFactory: otherwiseFactory));
+            Assert.True(invoked);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("\r\n")]
+        public void InvokesOtherwiseIfStringNullOrWhiteSpace(string test)
+        {
+            bool invoked = false;
+            const string otherwiseValue = "OTHERWISE";
+            string otherwiseFactory()
+            {
+                invoked = true;
+                return otherwiseValue;
+            }
+            Assert.Equal(otherwiseValue, test.IfNotNullOrWhiteSpace(otherwiseFactory: otherwiseFactory));
+            Assert.True(invoked);
+        }
+
+        [Fact]
+        public void ThrowsIfInstanceAndFactoryNull()
+        {
+            object test = null;
+            Assert.ThrowsAny<ArgumentNullException>(() => test.IfNotNull(otherwiseFactory: null));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ThrowsIfInstanceNullOrEmptyAndFactoryNull(string test)
+        {
+            Assert.ThrowsAny<ArgumentNullException>(() => test.IfNotNullOrEmpty(otherwiseFactory: null));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("\r\n")]
+        public void ThrowsIfInstanceNullOrWhitespaceAndFactoryNull(string test)
+        {
+            Assert.ThrowsAny<ArgumentNullException>(() => test.IfNotNullOrEmpty(otherwiseFactory: null));
         }
     }
 }
