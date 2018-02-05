47ca1fc178316abdf22b9e0d07d16948f32c925d
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2018-02-05 13:59:46 +0100

    Cleanup of previous CLI projects

--- sample/THNETII.AppFramework.Cli.Sample/Program.cs
+++ sample/THNETII.AppFramework.Cli.Sample/Program.cs
@@ -1,18 +1,12 @@
-﻿using McMaster.Extensions.CommandLineUtils;
-using McMaster.Extensions.CommandLineUtils.Validation;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using System;
 
 namespace THNETII.Common.Cli.Sample
 {
-    class Program
+    public static class Program
     {
         public static int Main(string[] args)
         {
-            return new CommandLineApplication()
-
-                .Execute(args ?? Array.Empty<string>());
+            return ProcessExitCode.ExitFailure;
         }
     }
 }
