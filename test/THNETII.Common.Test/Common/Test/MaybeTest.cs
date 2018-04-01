223c86a56a0387abe558f770d714084355bf087a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-04-02 00:29:31 +0200

    Added Maybe<T> simple constructor unit tests

---
+++ test/THNETII.Common.Test/Common/Test/MaybeTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace THNETII.Common.Test
+{
+    public static class MaybeTest
+    {
+        [Fact]
+        public static void NewHasNoValueAndThrows()
+        {
+            var maybe = new Maybe<string>();
+            Assert.False(maybe.HasValue);
+            Assert.Throws<InvalidOperationException>(() => maybe.Value);
+        }
+
+        [Fact]
+        public static void NewWithArgumentHasValue()
+        {
+            const string expect = nameof(NewWithArgumentHasValue);
+            var maybe = Maybe.Create(expect);
+            Assert.True(maybe.HasValue);
+            Assert.Equal(expect, maybe.Value);
+        }
+
+        [Fact]
+        public static void NewWithNullArgumentHasValue()
+        {
+            object value = null;
+            var maybe = Maybe.Create(value);
+            Assert.True(maybe.HasValue);
+            Assert.Null(maybe.Value);
+        }
+    }
+}
