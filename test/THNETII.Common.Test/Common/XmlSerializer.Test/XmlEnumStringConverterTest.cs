ee35082b2ac0af7ab5806505bc7651693eeb58dc
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-07-13 00:36:50 +0200

    Add XML Serializer Enum-String-Converter

---
+++ test/THNETII.Common.Test/Common/XmlSerializer.Test/XmlEnumStringConverterTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace THNETII.Common.XmlSerializer.Test
+{
+    public static class XmlEnumStringConverterTest
+    {
+        public const string One = nameof(One);
+        public const string Two = nameof(Two);
+        public const string Three = nameof(Three);
+        public const string Four = nameof(Four);
+
+        public enum TestEnum
+        {
+            [XmlEnum("One")]
+            First = 1,
+            [XmlEnum("Two")]
+            Second = 2,
+            [XmlEnum("Three")]
+            Third = 3,
+            [XmlEnum("Four")]
+            Fourth = 4,
+        }
+
+        [Theory]
+        [InlineData(nameof(TestEnum.First))]
+        [InlineData(nameof(TestEnum.Second))]
+        [InlineData(nameof(TestEnum.Third))]
+        [InlineData(nameof(TestEnum.Fourth))]
+        public static void CanParseEnumFieldName(string name)
+        {
+            Assert.Equal(Enum.Parse<TestEnum>(name), XmlEnumStringConverter<TestEnum>.Parse(name));
+        }
+
+        [Theory]
+        [InlineData(One, TestEnum.First)]
+        [InlineData(Two, TestEnum.Second)]
+        [InlineData(Three, TestEnum.Third)]
+        [InlineData(Four, TestEnum.Fourth)]
+        public static void CanParseEnumAttributeName(string name, TestEnum expected)
+        {
+            Assert.Equal(expected, XmlEnumStringConverter<TestEnum>.Parse(name));
+        }
+    }
+}
