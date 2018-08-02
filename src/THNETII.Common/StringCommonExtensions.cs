6dc51274f60b0518f2285a19ef3e640aff6c0edf
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-08-02 18:48:25 +0200

    Add string extension Contains with comparison type paramenter

---
+++ src/THNETII.Common/StringCommonExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace THNETII.Common
+{
+    public static class StringCommonExtensions
+    {
+        public static bool Contains(this string s, string value, StringComparison comparisonType)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            var v = value.AsSpan();
+            for (var remaining = s.AsSpan(); remaining.Length >= value.Length;
+                remaining = remaining.Slice(1))
+            {
+                if (remaining.StartsWith(v, comparisonType))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ test/THNETII.Common.Test/Common/Test/StringCommonExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace THNETII.Common.Test
+{
+    public class StringCommonExtensionsTest
+    {
+        [Fact]
+        public void ContainsThrowsWithNullSource()
+        {
+            const string test = null;
+            Assert.Throws<ArgumentNullException>(() => test.Contains("test", default));
+        }
+
+        [Fact]
+        public void ContainsThrowsWithNullValue()
+        {
+            const string test = "test";
+            Assert.Throws<ArgumentNullException>(() => test.Contains(null, default));
+        }
+
+        [Fact]
+        public void ContainsUsingOrdinalIgnoreCase()
+        {
+            const string source = "test";
+            string contains = source.ToUpperInvariant();
+
+            Assert.True(source.Contains(contains, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
