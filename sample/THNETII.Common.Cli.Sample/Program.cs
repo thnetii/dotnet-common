d2e1fc8f7a84b18e15beaa25876e14f2bcde9680
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-03 18:04:56 +0200

    Add async cancellable console utility class

---
+++ sample/THNETII.Common.Cli.Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace THNETII.Common.Cli.Sample
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConsoleUtils.RunAsync(args, (Func<string[], CancellationToken, Task>)MainAsync)
+                .GetAwaiter().GetResult();
+        }
+
+        public static async Task MainAsync(string[] args, CancellationToken cancelToken = default)
+        {
+            await Task.Yield();
+            var line = Console.ReadLine();
+            cancelToken.ThrowIfCancellationRequested();
+            Console.WriteLine("Read line: " + line ?? string.Empty);
+        }
+    }
+}
---
+++ src/THNETII.Common.Cli/ConsoleUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace THNETII.Common.Cli
+{
+    public static class ConsoleUtils
+    {
+        public static Task RunAsync(Func<CancellationToken, Task> asyncMain)
+        {
+            return RunAsync(Array.Empty<string>(), (asyncMain is null)
+                ? default(Func<string[], CancellationToken, Task<int>>)
+                : async (_, cancelToken) =>
+                {
+                    await asyncMain(cancelToken)
+                        .ConfigureAwait(continueOnCapturedContext: true);
+                    return ProcessExitCode.ExitSuccess;
+                });
+        }
+
+        public static Task RunAsync(string[] args, Func<string[], CancellationToken, Task> asyncMain)
+        {
+            return RunAsync(args, (asyncMain is null)
+                ? default(Func<string[], CancellationToken, Task<int>>)
+                : async (argvs, cancelToken) =>
+            {
+                await asyncMain(argvs, cancelToken)
+                    .ConfigureAwait(continueOnCapturedContext: true);
+                return ProcessExitCode.ExitSuccess;
+            });
+        }
+
+        public static Task<int> RunAsync(Func<CancellationToken, Task<int>> asyncMain) =>
+            RunAsync(Array.Empty<string>(), (asyncMain is null)
+                ? default(Func<string[], CancellationToken, Task<int>>)
+                : (args, cancelToken) => asyncMain(cancelToken));
+
+        public static async Task<int> RunAsync(string[] args, Func<string[], CancellationToken, Task<int>> asyncMain)
+        {
+            if (asyncMain is null)
+                throw new ArgumentNullException(nameof(asyncMain));
+
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                // If cancellation already has been requested,
+                // do not cancel process termination signal.
+                e.Cancel = !cts.IsCancellationRequested;
+
+                cts.Cancel(throwOnFirstException: true);
+            };
+
+            var cancelToken = cts.Token;
+            var returnValue = await asyncMain(args, cancelToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+            cancelToken.ThrowIfCancellationRequested();
+            return returnValue;
+        }
+    }
+}
