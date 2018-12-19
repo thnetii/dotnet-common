da65a1a4b7ac8f101433ed5294e885432bdab371
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-12-19 22:54:28 +0100

    Add initial Base64 URL Encode CLI tool

---
+++ src/THNETII.Base64UrlEncode.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.Reflection;
+
+namespace THNETII.Base64UrlEncode
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var programAssembly = typeof(Program).Assembly;
+            var app = new CommandLineApplication
+            {
+                AllowArgumentSeparator = true,
+                FullName = programAssembly.GetName().Name,
+                Description = programAssembly
+                    .GetCustomAttribute<AssemblyDescriptionAttribute>()?
+                    .Description
+            };
+            app.VersionOptionFromAssemblyAttributes(programAssembly);
+            app.HelpOption();
+
+            var decode = app.Option("-d|--decode", "Decode input", CommandOptionType.NoValue);
+            var stringEncoding = app.Option("--string-encoding", "Byte encoding (default: UTF-8)", CommandOptionType.SingleValue);
+            
+        }
+    }
+}
