60dce2450ec10b8211d1899302431f09a52befd2
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-12-20 01:05:04 +0100

    Add SemaphoreSlimContext class

---
+++ src/THNETII.Common/Threading/SemaphoreSlimContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace THNETII.Common.Threading
+{
+    public class SemaphoreSlimContext : IDisposable
+    {
+        private readonly SemaphoreSlim semaphore;
+
+        protected int ReleaseCount { get; }
+
+        private SemaphoreSlimContext(SemaphoreSlim semaphore, int releaseCount, bool verify)
+        {
+            Debug.Assert(!verify, nameof(verify) + " is not false.");
+            this.semaphore = semaphore;
+            ReleaseCount = releaseCount;
+        }
+
+        protected SemaphoreSlimContext(SemaphoreSlim semaphore,
+            int releaseCount = 1)
+            : this(
+                  semaphore ?? throw new ArgumentNullException(nameof(semaphore)),
+                  releaseCount >= 1 ? releaseCount : throw new ArgumentOutOfRangeException(nameof(releaseCount), releaseCount, nameof(releaseCount) + " is less than 1"),
+                  verify: false
+                  )
+        { }
+
+        internal static SemaphoreSlimContext CreateNoVerify(SemaphoreSlim semaphore,
+            int releaseCount = 1) =>
+            new SemaphoreSlimContext(semaphore, releaseCount, verify: false);
+
+        #region IDisposable Support
+        private int disposedValue = 0; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Interlocked.Exchange(ref disposedValue, 1) == 0)
+            {
+                if (disposing)
+                {
+                    semaphore?.Release(ReleaseCount);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SemaphoreSlimContext()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
---
+++ src/THNETII.Common/Threading/SemaphoreSlimExtensions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace THNETII.Common.Threading
+{
+    public static class SemaphoreSlimExtensions
+    {
+        public static SemaphoreSlimContext WaitContext(this SemaphoreSlim semaphore)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            semaphore.Wait();
+            return SemaphoreSlimContext.CreateNoVerify(semaphore);
+        }
+
+        public static SemaphoreSlimContext WaitContext(this SemaphoreSlim semaphore,
+            int millisecondsTimeout)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = semaphore.Wait(millisecondsTimeout);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+
+        public static SemaphoreSlimContext WaitContext(this SemaphoreSlim semaphore,
+            TimeSpan timeout)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = semaphore.Wait(timeout);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+
+        public static SemaphoreSlimContext WaitContext(this SemaphoreSlim semaphore,
+            CancellationToken cancelToken)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            semaphore.Wait(cancelToken);
+            return SemaphoreSlimContext.CreateNoVerify(semaphore);
+        }
+
+        public static SemaphoreSlimContext WaitContext(this SemaphoreSlim semaphore,
+            int millisecondsTimeout, CancellationToken cancelToken)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = semaphore.Wait(millisecondsTimeout, cancelToken);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+
+        public static SemaphoreSlimContext WaitContext(this SemaphoreSlim semaphore,
+            TimeSpan timeout, CancellationToken cancelToken)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = semaphore.Wait(timeout, cancelToken);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+
+        public static async Task<SemaphoreSlimContext> WaitContextAsync(this SemaphoreSlim semaphore)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            await semaphore.WaitAsync()
+                .ConfigureAwait(continueOnCapturedContext: false);
+            return SemaphoreSlimContext.CreateNoVerify(semaphore);
+        }
+
+        public static async Task<SemaphoreSlimContext> WaitContextAsync(this SemaphoreSlim semaphore,
+            int millisecondsTimeout)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = await semaphore.WaitAsync(millisecondsTimeout)
+                .ConfigureAwait(continueOnCapturedContext: false);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+
+        public static async Task<SemaphoreSlimContext> WaitContextAsync(this SemaphoreSlim semaphore,
+            TimeSpan timeout)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = await semaphore.WaitAsync(timeout)
+                .ConfigureAwait(continueOnCapturedContext: false);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+
+        public static async Task<SemaphoreSlimContext> WaitContextAsync(this SemaphoreSlim semaphore,
+            CancellationToken cancelToken)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            await semaphore.WaitAsync(cancelToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+            return SemaphoreSlimContext.CreateNoVerify(semaphore);
+        }
+
+        public static async Task<SemaphoreSlimContext> WaitContextAsync(this SemaphoreSlim semaphore,
+            int millisecondsTimeout, CancellationToken cancelToken)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = await semaphore.WaitAsync(millisecondsTimeout, cancelToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+
+        public static async Task<SemaphoreSlimContext> WaitContextAsync(this SemaphoreSlim semaphore,
+            TimeSpan timeout, CancellationToken cancelToken)
+        {
+            if (semaphore is null)
+                throw new ArgumentNullException(nameof(semaphore));
+            var entered = await semaphore.WaitAsync(timeout, cancelToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+            return entered ? SemaphoreSlimContext.CreateNoVerify(semaphore) : null;
+        }
+    }
+}
