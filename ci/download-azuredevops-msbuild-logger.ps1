7e09fcb95751185f70d9e3f44514e5dc68b13520
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-01-22 17:45:43 +0100

    Update Azure DevOps build pipeline

--- ci/download-azuredevops-msbuild-logger.ps1
+++ ci/download-azuredevops-msbuild-logger.ps1
@@ -2,7 +2,11 @@
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $resp = Invoke-WebRequest -Uri "https://github.com/thnetii/azure-msbuild/releases/latest" -Method Head
-[uri]$tagUri = $resp.BaseResponse.ResponseUri
+if ($resp.BaseResponse | Get-Member ResponseUri) { # In older PowerShell (beause System.Net.Requests is used)
+    [uri]$tagUri = $resp.BaseResponse.ResponseUri
+} else { # In newer PowerShell Core (because System.Net.Http is used)
+    [uri]$tagUri = $resp.BaseResponse.RequestMessage.RequestUri
+}
 [string]$tagName = $tagUri.Segments | Select-Object -Last 1
 [string]$fileName = "THNETII.AzureDevOps.MSBuild.Logger.zip"
 $filePath = Join-Path $PSScriptRoot $fileName
