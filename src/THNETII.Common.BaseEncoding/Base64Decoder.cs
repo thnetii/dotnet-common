5b496bfc62b0feff54cd09104850fc713571ef8a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-13 17:24:54 +0100

    Provide stubs for all Base64 Decorder methods

--- src/THNETII.Common.BaseEncoding/Base64Decoder.cs
+++ src/THNETII.Common.BaseEncoding/Base64Decoder.cs
@@ -8,19 +8,57 @@ namespace THNETII.Common.BaseEncoding
         public Base64Decoder() : base()
         { }
 
+#if !NETSTANDARD1_3
+        public override unsafe int GetByteCount(char* chars, int count, bool flush)
+        {
+            if (chars == null)
+                throw new ArgumentNullException(nameof(chars));
+            return GetByteCount(new ReadOnlySpan<char>(chars, count), flush);
+        }
+#endif // !NETSTANDARD1_3
+
         public override int GetByteCount(char[] chars, int index, int count, bool flush)
+        {
+            if (chars is null)
+                throw new ArgumentNullException(nameof(chars));
+            return GetByteCount(new ReadOnlySpan<char>(chars, index, count), flush);
+        }
+
+        public int GetByteCount(ReadOnlySpan<char> chars, bool flush)
         {
             throw new NotImplementedException();
         }
+
+#if !NETSTANDARD1_3
+        public override unsafe int GetBytes(char* chars, int charCount,
+            byte* bytes, int byteCount, bool flush)
+        {
+            if (chars == null)
+                throw new ArgumentNullException(nameof(chars));
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+            return GetBytes(
+                new ReadOnlySpan<char>(chars, charCount),
+                new Span<byte>(bytes, byteCount),
+                flush);
+        }
+#endif // !NETSTANDARD1_3
 
         public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex, bool flush)
         {
-            throw new NotImplementedException();
+            if (chars is null)
+                throw new ArgumentNullException(nameof(chars));
+            if (bytes is null)
+                throw new ArgumentNullException(nameof(bytes));
+            return GetBytes(
+                new ReadOnlySpan<char>(chars, charIndex, charCount),
+                new Span<byte>(bytes, byteIndex, bytes.Length - byteIndex),
+                flush);
         }
 
-        public override void Convert(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex, int byteCount, bool flush, out int charsUsed, out int bytesUsed, out bool completed)
+        public int GetBytes(ReadOnlySpan<char> chars, Span<byte> bytes, bool flush)
         {
-            base.Convert(chars, charIndex, charCount, bytes, byteIndex, byteCount, flush, out charsUsed, out bytesUsed, out completed);
+            throw new NotImplementedException();
         }
     }
 }
