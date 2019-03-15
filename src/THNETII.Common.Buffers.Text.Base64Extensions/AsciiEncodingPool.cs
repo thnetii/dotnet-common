f72b05ed3c38b9152e8d4651f1b67c6a98ecb0eb
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-15 01:56:48 +0100

    Switch to System.Buffer.Text.Base64 instead

---
+++ src/THNETII.Common.Buffers.Text.Base64Extensions/AsciiEncodingPool.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Microsoft.Extensions.ObjectPool;
+
+namespace THNETII.Common.Buffers.Text
+{
+    internal class AsciiEncodingPool : IPooledObjectPolicy<Encoder>, IPooledObjectPolicy<Decoder>
+    {
+        private static readonly AsciiEncodingPool instance = new AsciiEncodingPool();
+        private readonly ObjectPool<Encoder> encoderPool;
+        private readonly ObjectPool<Decoder> decoderPool;
+
+        public AsciiEncodingPool()
+        {
+            encoderPool = new DefaultObjectPool<Encoder>(this);
+            decoderPool = new DefaultObjectPool<Decoder>(this);
+        }
+
+        public static ObjectPool<Encoder> EncoderPool => instance.encoderPool;
+
+        public static ObjectPool<Decoder> DecoderPool => instance.decoderPool;
+
+        Encoder IPooledObjectPolicy<Encoder>.Create() =>
+            Encoding.ASCII.GetEncoder();
+
+        Decoder IPooledObjectPolicy<Decoder>.Create() =>
+            Encoding.ASCII.GetDecoder();
+
+        bool IPooledObjectPolicy<Encoder>.Return(Encoder encoder)
+        {
+            if (encoder is null)
+                return false;
+            encoder.Reset();
+            return true;
+        }
+
+        bool IPooledObjectPolicy<Decoder>.Return(Decoder decorder)
+        {
+            if (decorder is null)
+                return false;
+            decorder.Reset();
+            return true;
+        }
+    }
+}
---
+++ src/THNETII.Common.Buffers.Text.Base64Extensions/Base64UrlSafe.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Buffers;
+using System.Buffers.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace THNETII.Common.Buffers.Text
+{
+    public static class Base64UrlSafe
+    {
+        [SuppressMessage("Usage", "PC001: API not supported on all platforms", Justification = "https://github.com/dotnet/platform-compat/issues/123")]
+        public static OperationStatus DecodeFromChars(
+            char[] chars, int charIndex, int charCount,
+            Span<byte> bytes, out int charsConsumed, out int bytesWritten,
+            bool isFinalBlock = true, bool scrubBuffer = true)
+        {
+            RevertUrlSafe(
+                new Span<char>(chars, charIndex, charCount),
+                out int charsEnd, isFinalBlock);
+            var opStatus = Base64Utf16.DecodeFromChars(
+                chars, charIndex, charCount - charsEnd,
+                bytes, out charsConsumed, out bytesWritten,
+                isFinalBlock, scrubBuffer);
+            if (opStatus != OperationStatus.Done)
+                return opStatus;
+            unsafe
+            {
+                char* lastGroup = stackalloc char[] { '=', '=', '=', '=' };
+                for (int chIdx = charIndex + charsConsumed, gIdx = 0; gIdx < 4; gIdx++, chIdx++)
+                {
+                    lastGroup[gIdx] = chars[chIdx];
+                }
+                opStatus = Base64Utf16.DecodeFromChars(
+                    lastGroup, 4, bytes.Slice(bytesWritten),
+                    out int gConsumed, out int gWritten,
+                    isFinalBlock, scrubBuffer);
+                charsConsumed += charsEnd;
+                bytesWritten += gWritten;
+            }
+            return opStatus;
+        }
+
+        [SuppressMessage("Usage", "PC001: API not supported on all platforms", Justification = "https://github.com/dotnet/platform-compat/issues/123")]
+        public static unsafe OperationStatus DecodeFromChars(
+            char* chars, int charCount,
+            Span<byte> bytes, out int charsConsumed, out int bytesWritten,
+            bool isFinalBlock = true, bool scrubBuffer = true)
+        {
+            RevertUrlSafe(
+                new Span<char>(chars, charCount),
+                out int charsEnd, isFinalBlock);
+            var opStatus = Base64Utf16.DecodeFromChars(
+                chars, charCount - charsEnd,
+                bytes, out charsConsumed, out bytesWritten,
+                isFinalBlock, scrubBuffer);
+            if (opStatus != OperationStatus.Done)
+                return opStatus;
+            char* lastGroup = stackalloc char[] { '=', '=', '=', '=' };
+            for (int chIdx = charsConsumed, gIdx = 0; gIdx < 4; gIdx++, chIdx++)
+            {
+                lastGroup[gIdx] = chars[chIdx];
+            }
+            opStatus = Base64Utf16.DecodeFromChars(
+                lastGroup, 4, bytes.Slice(bytesWritten),
+                out int gConsumed, out int gWritten,
+                isFinalBlock, scrubBuffer);
+            charsConsumed += charsEnd;
+            bytesWritten += gWritten;
+            return opStatus;
+        }
+
+        [SuppressMessage("Usage", "PC001: API not supported on all platforms", Justification = "https://github.com/dotnet/platform-compat/issues/123")]
+        private static void RevertUrlSafe(Span<char> chars, out int endChars,
+            bool isFinalBlock = true)
+        {
+            Span<char> tmp = chars;
+            for (int idx = tmp.IndexOfAny('+', '/'); idx >= 0;
+                tmp = tmp.Slice(idx + 1), idx = tmp.IndexOfAny('+', '/'))
+            {
+                switch (tmp[idx])
+                {
+                    case '-':
+                        tmp[idx] = '+';
+                        break;
+                    case '_':
+                        tmp[idx] = '/';
+                        break;
+                }
+            }
+            if (isFinalBlock)
+                endChars = chars.Length % 4;
+            else
+                endChars = 0;
+        }
+
+        public static int GetMaxDecodedFromCharCount(int charCount) =>
+            Base64.GetMaxDecodedFromUtf8Length(charCount);
+
+        public static OperationStatus EncodeToChars(
+            ReadOnlySpan<byte> bytes, char[] chars, int charIndex,
+            out int bytesConsumed, out int charsWritten,
+            bool isFinalBlock = true, bool scrubBuffer = true)
+        {
+            var opStatus = Base64Utf16.EncodeToChars(
+                bytes, chars, charIndex,
+                out bytesConsumed, out charsWritten,
+                isFinalBlock, scrubBuffer);
+            charsWritten = MakeUrlSafe(
+                new Span<char>(chars, charIndex, charsWritten));
+            return opStatus;
+        }
+
+        public static unsafe OperationStatus EncodeToChars(
+            ReadOnlySpan<byte> bytes, char* chars, int charCount,
+            out int bytesConsumed, out int charsWritten,
+            bool isFinalBlock = true, bool scrubBuffer = true)
+        {
+            var opStatus = Base64Utf16.EncodeToChars(
+                bytes, chars, charCount,
+                out bytesConsumed, out charsWritten,
+                isFinalBlock, scrubBuffer);
+            charsWritten = MakeUrlSafe(
+                new Span<char>(chars, charsWritten));
+            return opStatus;
+        }
+
+        [SuppressMessage("Usage", "PC001: API not supported on all platforms", Justification = "https://github.com/dotnet/platform-compat/issues/123")]
+        private static int MakeUrlSafe(Span<char> chars)
+        {
+            Span<char> tmp = chars;
+            for (int idx = tmp.IndexOfAny('+', '/'); idx >= 0;
+                tmp = tmp.Slice(idx + 1), idx = tmp.IndexOfAny('+', '/'))
+            {
+                switch (tmp[idx])
+                {
+                    case '+':
+                        tmp[idx] = '-';
+                        break;
+                    case '/':
+                        tmp[idx] = '_';
+                        break;
+                }
+            }
+            for (int idx = chars.Length - 1; idx >= 0; idx--)
+            {
+                if (tmp[idx] != '=')
+                    return idx + 1;
+            }
+            return 0;
+        }
+
+        public static int GetMaxEncodedCharCount(int length) =>
+            Base64.GetMaxEncodedToUtf8Length(length);
+    }
+}
