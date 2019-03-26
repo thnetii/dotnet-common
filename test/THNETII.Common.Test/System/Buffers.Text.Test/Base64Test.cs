e2a6fa52e804f30ba6ff6795cb237a5ea5c5f412
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-26 21:56:59 +0100

    Add Base64 unit tests

---
+++ test/THNETII.Common.Test/System/Buffers.Text.Test/Base64Test.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace System.Buffers.Text.Test
+{
+    public static class Base64Test
+    {
+        [Fact]
+        public static void Base64_DecodeFromUtf8_with_empty_input()
+        {
+            var status = Base64.DecodeFromUtf8(ReadOnlySpan<byte>.Empty, Span<byte>.Empty, out int bytesConsumed, out int bytesWritten);
+
+            Assert.Equal(OperationStatus.Done, status);
+            Assert.Equal(0, bytesConsumed);
+            Assert.Equal(0, bytesWritten);
+        }
+    }
+}
---
+++ test/THNETII.Common.Test/System/Test/ConvertBase64TryTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+using Xunit;
+
+namespace System.Test
+{
+    public static class ConvertBase64TryTest
+    {
+        [Fact]
+        [SuppressMessage("Usage", "PC001: API not supported on all platforms", Justification = "https://github.com/dotnet/platform-compat/issues/123")]
+        public static void TryFromBase64String_returns_false_if_missing_padding()
+        {
+            const string test = "TWFuIG";
+            const string text = "Man";
+            Span<byte> bytes = stackalloc byte[test.Length];
+
+            bool success = Convert.TryFromBase64String(test, bytes, out int bytesWritten);
+
+            Assert.False(success);
+            Assert.Equal(3, bytesWritten);
+            Assert.Equal(text, Encoding.ASCII.GetString(bytes.Slice(bytesWritten)));
+        }
+    }
+}
