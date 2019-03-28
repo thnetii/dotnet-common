2eedb46707f32b15a219aee4928b87eaa8436dc2
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-28 09:21:07 +0100

    Refactor memory extensions

--- src/THNETII.Common.IO.Extensions/TextReaderPipelinesExtensions.cs
+++ src/THNETII.Common.IO.Extensions/TextReaderPipelinesExtensions.cs
@@ -81,45 +81,5 @@ namespace THNETII.Common.IO
                 .ConfigureAwait(false);
 
         }
-
-#if !NETCOREAPP
-        public static Task<int> ReadBlockAsync(this TextReader reader,
-            Memory<char> buffer, CancellationToken cancelToken = default)
-        {
-            if (reader is null)
-                throw new ArgumentNullException(nameof(reader));
-
-            cancelToken.ThrowIfCancellationRequested();
-            bool bufferIsArray = MemoryMarshal.TryGetArray(buffer, out ArraySegment<char> segment);
-            if (bufferIsArray)
-            {
-                return reader.ReadBlockAsync(segment.Array, segment.Offset, segment.Count);
-            }
-            else
-            {
-                return reader.ReadBlockThroughIntermediateBufferAsync(buffer, cancelToken);
-            }
-        }
-
-        private static async Task<int> ReadBlockThroughIntermediateBufferAsync(
-            this TextReader reader, Memory<char> buffer,
-            CancellationToken cancelToken = default)
-        {
-            ArrayPool<char> arrayPool = ArrayPool<char>.Shared;
-            char[] array = arrayPool.Rent(buffer.Length);
-            try
-            {
-                cancelToken.ThrowIfCancellationRequested();
-
-                var charsRead = await reader.ReadBlockAsync(array, 0, buffer.Length)
-                    .ConfigureAwait(false);
-                cancelToken.ThrowIfCancellationRequested();
-                var readMemory = new Memory<char>(array, 0, charsRead);
-                readMemory.CopyTo(buffer);
-                return charsRead;
-            }
-            finally { arrayPool.Return(array); }
-        }
-#endif // !NETCOREAPP
     }
 }
--- src/THNETII.Common.IO.Extensions/TextWriterPipelinesExtensions.cs
+++ src/THNETII.Common.IO.Extensions/TextWriterPipelinesExtensions.cs
@@ -74,44 +74,5 @@ namespace THNETII.Common.IO
                 bufferNext?.Dispose();
             }
         }
-
-#if !NETCOREAPP
-        public static Task WriteAsync(this TextWriter writer,
-            ReadOnlyMemory<char> buffer, CancellationToken cancelToken = default)
-        {
-            if (writer is null)
-                throw new ArgumentNullException(nameof(writer));
-
-            cancelToken.ThrowIfCancellationRequested();
-            bool bufferIsArray = MemoryMarshal.TryGetArray(buffer, out ArraySegment<char> segment);
-            if (bufferIsArray)
-            {
-                return writer.WriteAsync(segment.Array, segment.Offset, segment.Count);
-            }
-            else
-            {
-                return writer.WriteThroughIntermediateBufferAsync(buffer, cancelToken);
-            }
-        }
-
-        private static async Task WriteThroughIntermediateBufferAsync(
-            this TextWriter writer, ReadOnlyMemory<char> buffer,
-            CancellationToken cancelToken = default)
-        {
-            ArrayPool<char> arrayPool = ArrayPool<char>.Shared;
-            char[] array = arrayPool.Rent(buffer.Length);
-            try
-            {
-                cancelToken.ThrowIfCancellationRequested();
-
-                buffer.CopyTo(array);
-
-                cancelToken.ThrowIfCancellationRequested();
-                await writer.WriteAsync(array, 0, buffer.Length)
-                    .ConfigureAwait(false);
-            }
-            finally { arrayPool.Return(array); }
-        }
-#endif // !NETCOREAPP
     }
 }
