cd5e28294f20261c030230fb0b0c1ea50c4df09e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-03-30 19:54:38 +0100

    Add memory to array marshaler

---
+++ src/THNETII.Common.Buffers/ArrayDisposableSegment.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Buffers;
+using System.Threading;
+
+namespace THNETII.Common.Buffers
+{
+    public struct ArrayDisposableSegment<T> : IDisposable, IEquatable<ArrayDisposableSegment<T>>, IEquatable<ArraySegment<T>>
+    {
+        private ArraySegment<T> segment;
+        private IMemoryOwner<T> memoryOwner;
+        private Memory<T> originalMemory;
+
+        public ArraySegment<T> Segment => memoryOwner is null
+            ? throw new ObjectDisposedException(nameof(memoryOwner))
+            : segment;
+
+        public bool IsDisposable => memoryOwner is IDisposable;
+
+        internal ArrayDisposableSegment(T[] array,
+            IMemoryOwner<T> memoryOwner = null, Memory<T> originalMemory = default)
+            : this(new ArraySegment<T>(array), memoryOwner, originalMemory) { }
+
+        internal ArrayDisposableSegment(T[] array, int offset, int length,
+            IMemoryOwner<T> memoryOwner = null, Memory<T> originalMemory = default)
+            : this(new ArraySegment<T>(array, offset, length), memoryOwner, originalMemory) { }
+
+        internal ArrayDisposableSegment(ArraySegment<T> segment,
+            IMemoryOwner<T> memoryOwner = null, Memory<T> originalMemory = default)
+            : this()
+        {
+            (this.segment, this.memoryOwner, this.originalMemory) =
+                (segment, memoryOwner, originalMemory);
+        }
+
+        public void Dispose()
+        {
+            var memoryOwner = Interlocked.Exchange(ref this.memoryOwner, null);
+            segment = default;
+            Memory<T> originalMemory;
+            (originalMemory, this.originalMemory) = (this.originalMemory, default);
+            _ = memoryOwner?.Memory.TryCopyTo(originalMemory);
+            memoryOwner?.Dispose();
+        }
+
+        public override int GetHashCode() => segment.GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            switch (obj)
+            {
+                case ArrayDisposableSegment<T> other:
+                    return Equals(other);
+                case ArraySegment<T> other:
+                    return Equals(other);
+                default:
+                    return false;
+            }
+        }
+
+        public bool Equals(ArrayDisposableSegment<T> other)
+        {
+            return Equals(other.segment)
+                && memoryOwner.Equals(other.memoryOwner)
+                && originalMemory.Equals(other.originalMemory);
+        }
+
+        public bool Equals(ArraySegment<T> other) => segment.Equals(other);
+
+        public static bool operator ==(ArrayDisposableSegment<T> left, ArrayDisposableSegment<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ArrayDisposableSegment<T> left, ArrayDisposableSegment<T> right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(ArrayDisposableSegment<T> left, ArraySegment<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ArrayDisposableSegment<T> left, ArraySegment<T> right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator ==(ArraySegment<T> left, ArrayDisposableSegment<T> right)
+        {
+            return right.Equals(left);
+        }
+
+        public static bool operator !=(ArraySegment<T> left, ArrayDisposableSegment<T> right)
+        {
+            return !(right == left);
+        }
+    }
+}
---
+++ src/THNETII.Common.Buffers/ArrayMemoryMarshal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace THNETII.Common.Buffers
+{
+    public static class ArrayMemoryMarshal
+    {
+        public static ArrayDisposableSegment<T> GetMemoryArray<T>(
+            Memory<T> memory, bool noCopy = false, bool noCopyBack = true)
+        {
+            if (memory.IsEmpty)
+            {
+                return new ArrayDisposableSegment<T>(Array.Empty<T>(), 0, 0);
+            }
+            else if (MemoryMarshal.TryGetArray((ReadOnlyMemory<T>)memory, out var segment))
+            {
+                return new ArrayDisposableSegment<T>(segment);
+            }
+
+            var owner = ArrayMemoryPool<T>.Shared.Rent(memory.Length);
+            try
+            {
+                if (!noCopy)
+                    memory.CopyTo(owner.Memory);
+                Memory<T> copyMemory = owner.Memory.Slice(0, memory.Length);
+                bool isArray = MemoryMarshal.TryGetArray((ReadOnlyMemory<T>)copyMemory, out var segment);
+                if (!isArray) throw new InvalidOperationException("Memory returned from ArrayMemoryPool is not an array.");
+                return new ArrayDisposableSegment<T>(segment, owner,
+                    noCopyBack ? Memory<T>.Empty : memory);
+            }
+            catch (Exception) when (DisposeWithoutUnwind(owner)) { throw; }
+
+            bool DisposeWithoutUnwind(IDisposable disposable)
+            {
+                disposable?.Dispose();
+                return false; // Return false to not catch exception
+            }
+        }
+
+        public static ArrayDisposableSegment<T> GetMemoryArray<T>(
+            ReadOnlyMemory<T> memory, bool noCopy = false)
+        {
+            if (memory.IsEmpty)
+            {
+                return new ArrayDisposableSegment<T>(Array.Empty<T>(), 0, 0);
+            }
+            else if (MemoryMarshal.TryGetArray(memory, out var segment))
+            {
+                return new ArrayDisposableSegment<T>(segment);
+            }
+
+            var owner = ArrayMemoryPool<T>.Shared.Rent(memory.Length);
+            try
+            {
+                if (!noCopy)
+                    memory.CopyTo(owner.Memory);
+                Memory<T> copyMemory = owner.Memory.Slice(0, memory.Length);
+                bool isArray = MemoryMarshal.TryGetArray((ReadOnlyMemory<T>)copyMemory, out var segment);
+                if (!isArray) throw new InvalidOperationException("Memory returned from ArrayMemoryPool is not an array.");
+                return new ArrayDisposableSegment<T>(segment, owner);
+            }
+            catch (Exception) when (DisposeWithoutUnwind(owner)) { throw; }
+
+            bool DisposeWithoutUnwind(IDisposable disposable)
+            {
+                disposable?.Dispose();
+                return false; // Return false to not catch exception
+            }
+        }
+    }
+}
