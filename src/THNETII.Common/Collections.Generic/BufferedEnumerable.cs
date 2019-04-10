fd3d47729c45d0efa0d2c7378d23ba49de7f3858
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-04-10 17:08:43 +0200

    Buffered Enumerable

---
+++ src/THNETII.Common/Collections.Generic/BufferedEnumerable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace THNETII.Common.Collections.Generic
+{
+    /// <summary>
+    /// Wrapper around an <see cref="IEnumerable{T}"/> interface that provides
+    /// iteration using an <see cref="IEnumerator{T}"/> that supports <see cref="IEnumerator.Reset"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+    [SuppressMessage("Naming", "CA1710: Identifiers should have correct suffix", Justification = "Type is not a collection, Enumerable suffix is by design.")]
+    public class BufferedEnumerable<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> enumerable;
+
+        /// <summary>
+        /// Wraps the specified enumerable into a buffered <see cref="IEnumerable{T}"/> interface
+        /// that allows for iteration using an <see cref="IEnumerator{T}"/> that supports <see cref="IEnumerator.Reset"/>.
+        /// </summary>
+        /// <param name="enumerable">The enumerable to wrap. Must not be <see langword="null"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="enumerable"/> is <see langword="null"/>.</exception>
+        public BufferedEnumerable(IEnumerable<T> enumerable) : base()
+        {
+            this.enumerable = enumerable ?? throw new ArgumentNullException(nameof(enumerable));
+        }
+
+        /// <summary>
+        /// Returns a buffered enumerator that iterates through the enumerable
+        /// and supports <see cref="IEnumerator.Reset"/>.
+        /// </summary>
+        /// <returns>A buffered, disposable enumerator that can be used to iterate through the enumerable.</returns>
+        /// <remarks>
+        /// The returned <see cref="IEnumerator{T}"/> instance is guaranteed to
+        /// support the <see cref="IEnumerator.Reset"/> method.
+        /// <para>
+        /// The enumerator maintains an internal buffer to which it adds the
+        /// current element when <see cref="IEnumerator.MoveNext"/> is called.
+        /// </para>
+        /// <para>
+        /// When <see cref="IEnumerator.Reset"/> is called on the returned
+        /// enumerator, the underlying <see cref="IEnumerator{T}"/> from the
+        /// original enumerable is disposed and replaced by a new
+        /// <see cref="IEnumerator{T}"/> obtained from the internal buffer.
+        /// </para>
+        /// </remarks>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new BufferedEnumerator<T>(
+                enumerable.GetEnumerator(),
+                (enumerable as ICollection<T>)?.Count
+                );
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    /// <summary>Provides buffered enumable extensions for <see cref="IEnumerable{T}"/> instances.</summary>
+    /// <seealso cref="BufferedEnumerable{T}"/>
+    public static class BufferedEnumerable
+    {
+        /// <inheritdoc cref="BufferedEnumerable{T}(IEnumerable{T})"/>
+        public static BufferedEnumerable<T> AsBufferedEnumerable<T>(this IEnumerable<T> enumerable) =>
+            new BufferedEnumerable<T>(enumerable);
+    }
+}
---
+++ src/THNETII.Common/Collections.Generic/BufferedEnumerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace THNETII.Common.Collections.Generic
+{
+    internal sealed class BufferedEnumerator<T> : IEnumerator<T>
+    {
+        internal IEnumerator<T> Enumerator => enumerator ?? throw new ObjectDisposedException(nameof(enumerator));
+
+        internal ICollection<T> Buffer => buffer ?? throw new ObjectDisposedException(nameof(buffer));
+
+        public T Current => Enumerator.Current;
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            var enumerator = Enumerator;
+            if (enumerator.MoveNext())
+            {
+                Buffer.Add(enumerator.Current);
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            var (previousEnumerator, previousBuffer) = (Enumerator, Buffer);
+            (enumerator, buffer) = (previousBuffer.GetEnumerator(), new List<T>(previousBuffer.Count));
+            previousEnumerator.Dispose();
+        }
+
+        private IEnumerator<T> enumerator;
+        private ICollection<T> buffer;
+
+        /// <summary>
+        /// Creates a new wrapper aorund the specified enumerable, optionally
+        /// with an indication of the number of exepected enumerabled items for
+        /// optimal allocation of the internal buffer.
+        /// </summary>
+        /// <param name="enumerator">The <see cref="IEnumerator{T}"/> to wrap. Must not be <see langword="null"/>.</param>
+        /// <param name="capacity">Optional. The initial capacity for the internal buffer.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="enumerator"/> is <see langword="null"/>.</exception>
+        public BufferedEnumerator(IEnumerator<T> enumerator, int? capacity = null)
+            : this(enumerator, capacity.HasValue ? new List<T>(capacity.Value) : new List<T>())
+        { }
+
+        internal BufferedEnumerator(IEnumerator<T> enumerator, ICollection<T> buffer)
+        {
+            this.enumerator = enumerator ?? throw new ArgumentNullException(nameof(enumerator));
+            this.buffer = buffer ?? throw new ArgumentNullException(nameof(buffer));
+        }
+
+        public void Dispose()
+        {
+            Interlocked.Exchange(ref enumerator, null)?.Dispose();
+            if (Interlocked.Exchange(ref this.buffer, null).TryNotNull(out var buffer))
+            {
+                buffer.Clear();
+            }
+        }
+    }
+}
