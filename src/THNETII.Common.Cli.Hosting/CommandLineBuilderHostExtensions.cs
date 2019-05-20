c204c22be64fa875820474a6192a2186d82b940a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-20 11:48:41 +0200

    Add CLI Hosting Extensions project

---
+++ src/THNETII.Common.Cli.Hosting/CommandLineBuilderHostExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.Linq;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace THNETII.Common.Cli.Hosting
+{
+    public static class CommandLineBuilderHostExtensions
+    {
+        public static CommandLineBuilder UseHost(this CommandLineBuilder commandLine,
+            Func<string[], IHostBuilder> createHostBuilder)
+        {
+            return commandLine.UseMiddleware((context, next) =>
+            {
+                return createHostBuilder(context.ParseResult.UnmatchedTokens.ToArray())
+                    .ConfigureServices(services =>
+                    {
+                        services.AddSingleton(context);
+                        services.AddSingleton(context.BindingContext);
+                        services.AddSingleton(context.Console);
+                        services.AddSingleton(context.ParseResult);
+                    })
+                    .RunConsoleAsync();
+            });
+        }
+    }
+}
