4a3b7ad3e3d4ae8ca5b583f4af7b41436513ab6c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-06-19 12:41:57 +0200

    Update THNETII.CommandLine.Hosting

--- src/THNETII.CommandLine.Hosting/HostingExtensions.cs
+++ src/THNETII.CommandLine.Hosting/HostingExtensions.cs
@@ -1,25 +1,31 @@
-﻿using System;
-using System.CommandLine.Builder;
+﻿using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
 using System.Linq;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
-namespace THNETII.CommandLine.Hosting
+namespace System.CommandLine.Hosting
 {
     public static class HostingExtensions
     {
+        public const string ConfigurationDirectiveName = "config";
+
         public static CommandLineBuilder UseHost(this CommandLineBuilder builder,
             Func<string[], IHostBuilder> hostBuilderFactory,
             Action<IHostBuilder> configureHost = null) =>
             builder.UseMiddleware(async (invocation, next) =>
             {
-                var argsRemaining = invocation.ParseResult.UnmatchedTokens.ToArray();
+                var argsRemaining = invocation.ParseResult.UnparsedTokens.ToArray();
                 var hostBuilder = hostBuilderFactory?.Invoke(argsRemaining)
                     ?? new HostBuilder();
                 hostBuilder.Properties[typeof(InvocationContext)] = invocation;
 
+                hostBuilder.ConfigureHostConfiguration(config =>
+                {
+                    config.AddCommandLineDirectives(invocation.ParseResult, ConfigurationDirectiveName);
+                });
                 hostBuilder.ConfigureServices(services =>
                 {
                     services.AddSingleton(invocation);
@@ -28,7 +34,7 @@ namespace THNETII.CommandLine.Hosting
                     services.AddTransient(_ => invocation.InvocationResult);
                     services.AddTransient(_ => invocation.ParseResult);
                 });
-                hostBuilder.UseConsoleLifetime();
+                hostBuilder.UseInvocationLifetime(invocation);
                 configureHost?.Invoke(hostBuilder);
 
                 using (var host = hostBuilder.Build())
@@ -46,5 +52,17 @@ namespace THNETII.CommandLine.Hosting
         public static CommandLineBuilder UseHost(this CommandLineBuilder builder,
             Action<IHostBuilder> configureHost = null
             ) => UseHost(builder, null, configureHost);
+
+        public static IHostBuilder UseInvocationLifetime(this IHostBuilder host,
+            InvocationContext invocation, Action<InvocationLifetimeOptions> configureOptions = null)
+        {
+            return host.ConfigureServices(services =>
+            {
+                services.TryAddSingleton(invocation);
+                services.AddSingleton<IHostLifetime, InvocationLifetime>();
+                if (configureOptions is Action<InvocationLifetimeOptions>)
+                    services.Configure(configureOptions);
+            });
+        }
     }
 }
