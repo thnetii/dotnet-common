3b099de6c598365bc511ac6a1982f0c5984f1fa9
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-10-18 00:38:39 +0200

    Add default hosted command-line invocation helper

---
+++ src/THNETII.CommandLine.Hosting/ICommandDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.CommandLine;
+
+namespace THNETII.CommandLine.Hosting
+{
+    /// <summary>
+    /// The base interface for an application specific definition of a
+    /// command-line root command.
+    /// </summary>
+    public interface ICommandDefinition
+    {
+        /// <summary>
+        /// Gets the root command for the application specific command-line.
+        /// </summary>
+        Command RootCommand { get; }
+    }
+}
---
+++ src/THNETII.CommandLine.Hosting/InvocationLifetimeOptionsPostConfigure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+using System.CommandLine.Hosting;
+using System.Diagnostics.CodeAnalysis;
+
+namespace THNETII.CommandLine.Hosting
+{
+    [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes", Justification = nameof(Microsoft.Extensions.DependencyInjection))]
+    internal class InvocationLifetimeOptionsPostConfigure : IPostConfigureOptions<InvocationLifetimeOptions>
+    {
+        private readonly IConfiguration configuration;
+
+        public InvocationLifetimeOptionsPostConfigure(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void PostConfigure(string name, InvocationLifetimeOptions options)
+            => configuration.Bind("Lifetime", options);
+    }
+}
