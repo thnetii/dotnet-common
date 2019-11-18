7fe87da10963bcf3ce5080f24cb8d9a6b4c4cd78
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2019-11-18 15:34:50 +0100

    Add documentation for HTTP URL helper

--- src/THNETII.Networking.Http/HttpUrlHelper.cs
+++ src/THNETII.Networking.Http/HttpUrlHelper.cs
@@ -6,11 +6,31 @@ using THNETII.Common;
 
 namespace THNETII.Networking.Http
 {
+    /// <summary>
+    /// Helper class that provides useful helper methods in constructing URLs
+    /// and query strings.
+    /// </summary>
     public static class HttpUrlHelper
     {
         private static StringBuilder? queryBuilder;
 
-        public static string ToQueryString(IEnumerable<KeyValuePair<string, string>>? queryParams)
+        /// <summary>
+        /// Constructs a query string using the specified enumerable of
+        /// string-typed key-value-pairs. Each value is URI-escaped as URI data.
+        /// </summary>
+        /// <param name="queryParams">
+        /// A sequence of string-typed key-value-pairs. Multiple equal key
+        /// values are allowed. <see langword="null"/> values are allowed.
+        /// The sequence can be <see langword="null"/>.
+        /// </param>
+        /// <returns>
+        /// A non-<see langword="null"/> <see cref="string"/> starting with the
+        /// leading <c>'?'</c>. If <paramref name="queryParams"/> contains any
+        /// pairs, the pair is added to the query string. Pairs where the key is
+        /// either <see langword="null"/>, empty or contains only whitespace are
+        /// skipped regardless of the value of the pair.
+        /// </returns>
+        public static string ToQueryString(IEnumerable<KeyValuePair<string, string?>>? queryParams)
         {
             if (queryParams is null)
                 return "?";
