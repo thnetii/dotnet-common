6573679073000b2d019ac774f035707b6146f36a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-03-04 07:51:53 +0100

    Add documentation for Command-line parser mapping configuration source

--- src/THNETII.CommandLine.Hosting/CommandLineParseResultConfigurationSource.cs
+++ src/THNETII.CommandLine.Hosting/CommandLineParseResultConfigurationSource.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Parsing;
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Configuration.Memory;
 
 namespace THNETII.CommandLine.Hosting
 {
+    /// <summary>
+    /// A configuration source implementation that maps a <see cref="ParseResult"/>
+    /// to configuration keys.
+    /// </summary>
     public class CommandLineParseResultConfigurationSource : IConfigurationSource
     {
         private readonly ParseResult parseResult;
@@ -20,10 +24,25 @@ namespace THNETII.CommandLine.Hosting
             this.parseResult = parseResult;
         }
 
+        /// <inheritdoc cref="MapOption(string, IOption, Func{OptionResult, string?})"/>
         public CommandLineParseResultConfigurationSource MapOption(string key,
             Option<string> option) =>
             MapOption(key, option, r => r.GetValueOrDefault<string>());
 
+        /// <summary>
+        /// Maps a command-line option to the specified configuration key.
+        /// </summary>
+        /// <param name="key">The configuration key to map to.</param>
+        /// <param name="option">
+        /// The option to map the result from.
+        /// If there is no result for the specified option in the <see cref="ParseResult"/>
+        /// no configuration key will be added.
+        /// </param>
+        /// <param name="resultToValue">Specifies a mapping function that maps <see cref="OptionResult"/> to the configuration value.</param>
+        /// <returns>The current <see cref="IConfigurationSource"/> instance to allow fluid chaing of method calls.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="key"/>, <paramref name="option"/> or <paramref name="resultToValue"/> is <see langword="null"/>.
+        /// </exception>
         public CommandLineParseResultConfigurationSource MapOption(string key,
             IOption option, Func<OptionResult, string?> resultToValue)
         {
@@ -42,6 +61,7 @@ namespace THNETII.CommandLine.Hosting
             return this;
         }
 
+        /// <inheritdoc cref="MapOption(string, IOption, Func{OptionResult, string?})"/>
         public CommandLineParseResultConfigurationSource MapOption(string key,
             IOption option, Func<OptionResult, IEnumerable<string?>?> resultToValue)
         {
@@ -64,10 +84,25 @@ namespace THNETII.CommandLine.Hosting
             return this;
         }
 
+        /// <inheritdoc cref="MapOption(string, IOption, Func{OptionResult, string?})"/>
         public CommandLineParseResultConfigurationSource MapArgument(string key,
             Argument<string> argument) =>
             MapArgument(key, argument, r => r.GetValueOrDefault<string>());
 
+        /// <summary>
+        /// Maps a command-line argument to the specified configuration key.
+        /// </summary>
+        /// <param name="key">The configuration key to map to.</param>
+        /// <param name="argument">
+        /// The argument to map the result from.
+        /// If there is no result for the specified option in the <see cref="ParseResult"/>
+        /// no configuration key will be added.
+        /// </param>
+        /// <param name="resultToValue">Specifies a mapping function that maps <see cref="ArgumentResult"/> to the configuration value.</param>
+        /// <returns>The current <see cref="IConfigurationSource"/> instance to allow fluid chaing of method calls.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="key"/>, <paramref name="argument"/> or <paramref name="resultToValue"/> is <see langword="null"/>.
+        /// </exception>
         public CommandLineParseResultConfigurationSource MapArgument(string key,
             IArgument argument, Func<ArgumentResult, string?> resultToValue)
         {
@@ -86,6 +121,7 @@ namespace THNETII.CommandLine.Hosting
             return this;
         }
 
+        /// <inheritdoc cref="MapOption(string, IOption, Func{OptionResult, string?})"/>
         public CommandLineParseResultConfigurationSource MapArgument(string key,
             IArgument argument, Func<ArgumentResult, IEnumerable<string?>?> resultToValue)
         {
--- src/THNETII.CommandLine.Hosting/ConfigurationExtensions.cs
+++ src/THNETII.CommandLine.Hosting/ConfigurationExtensions.cs
@@ -1,28 +1,61 @@
-﻿using System;
+using System;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
+using System.Diagnostics.CodeAnalysis;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace THNETII.CommandLine.Hosting
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="IConfiguration"/>.
+    /// </summary>
     public static class ConfigurationExtensions
     {
+        /// <summary>
+        /// Add a source that maps configuration entries from the command-line
+        /// parser <see cref="ParseResult"/> using the specified host builder
+        /// context.
+        /// </summary>
+        /// <param name="configBuilder">The configuration builder to add the configuration source to.</param>
+        /// <param name="context">The current host builder context providing the <see cref="ParseResult"/>.</param>
+        /// <param name="sourceActions">A method or lambda expression specifying how command-line arguments are to be translates to configuration keys.</param>
+        /// <returns><paramref name="configBuilder"/> to enable the possibility for chaining method calls.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="configBuilder"/> or <paramref name="context"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="context"/> does not provide an <see cref="InvocationContext"/>.
+        /// </exception>
+        [SuppressMessage("Globalization", "CA1303: Do not pass literals as localized parameters")]
         public static IConfigurationBuilder AddCommandLineParseResult(
             this IConfigurationBuilder configBuilder,
-            HostBuilderContext context, 
+            HostBuilderContext context,
             Action<CommandLineParseResultConfigurationSource>? sourceActions = null)
         {
             _ = context ?? throw new ArgumentNullException(nameof(context));
 
             _ = context.Properties.TryGetValue(typeof(InvocationContext),
                 out object invocationObj);
+            _ = invocationObj ?? throw new ArgumentException("The provided host builder context does not provide an Command-line Parser Invocation context.", nameof(context));
 
             return AddCommandLineParseResult(configBuilder,
                 (InvocationContext)invocationObj, sourceActions);
         }
 
+        /// <summary>
+        /// Add a source that maps configuration entries from the command-line
+        /// parser <see cref="ParseResult"/> using the specified invocation
+        /// context.
+        /// </summary>
+        /// <param name="configBuilder">The configuration builder to add the configuration source to.</param>
+        /// <param name="context">The command-line parser invocation context providing the <see cref="ParseResult"/>.</param>
+        /// <param name="sourceActions">A method or lambda expression specifying how command-line arguments are to be translates to configuration keys.</param>
+        /// <returns><paramref name="configBuilder"/> to enable the possibility for chaining method calls.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="configBuilder"/> or <paramref name="context"/> is <see langword="null"/>.
+        /// </exception>
         public static IConfigurationBuilder AddCommandLineParseResult(
             this IConfigurationBuilder configBuilder,
             InvocationContext context,
@@ -31,6 +64,17 @@ namespace THNETII.CommandLine.Hosting
                 (context ?? throw new ArgumentNullException(nameof(context)))
                 .ParseResult, sourceActions);
 
+        /// <summary>
+        /// Add a source that maps configuration entries from the command-line
+        /// parser <see cref="ParseResult"/>.
+        /// </summary>
+        /// <param name="configBuilder">The configuration builder to add the configuration source to.</param>
+        /// <param name="parseResult">The command-line parser result to map configuration keys from.</param>
+        /// <param name="sourceActions">A method or lambda expression specifying how command-line arguments are to be translates to configuration keys.</param>
+        /// <returns><paramref name="configBuilder"/> to enable the possibility for chaining method calls.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="configBuilder"/> or <paramref name="parseResult"/> is <see langword="null"/>.
+        /// </exception>
         public static IConfigurationBuilder AddCommandLineParseResult(
             this IConfigurationBuilder configBuilder, ParseResult parseResult,
             Action<CommandLineParseResultConfigurationSource>? sourceActions = null)
